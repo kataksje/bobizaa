572bab4397b79b20542170fd785ac5953ef93a70
kataksje <164706808+kataksje@users.noreply.github.com>
2024-03-24 10:46:56 +0300

    Update tz.js

--- plugins/tz.js
+++ plugins/tz.js
@@ -3,13 +3,13 @@ import moment from 'moment-timezone';
 const handler = async (m, {conn}) => {
 
   const tzAF = moment().tz('Africa/Casablanca').format('DD/MM HH:mm');
-  await conn.sendMessage(m.chat, {text: `\`\`\`
-الوقت الحالي في المغرب هو :
+  await conn.sendMessage(m.chat, {text: `\`\`\` 
+   هو في الوقت الحالي في اليمن :
 
 
 ▢ morocco     : ${tzAF}
   ${String.fromCharCode(8206).repeat(850)}
-  ▢ instagram.com/noureddine_ouafy`}, {quoted: m});
+  ▢ https://wa.me/+967730068059`}, {quoted: m});
   };
 handler.help = ["tz"]
 handler.tags = ["infobot"]
